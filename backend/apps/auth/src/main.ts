5cfb3942bc0a3c4437a8a9066363a3a33a40a468
Guillem Casas <gucasassi@gmail.com>
2023-12-01 23:43:05 +0100

    chore(back): add env files on auth microservice

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -1,14 +1,22 @@
+// Imports
+import dotenv from 'dotenv';
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+// Load env variables
+dotenv.config();
 
+// Initialize expres app
 const app = express();
 
+// Routes
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ success: true, message: 'Hi there!' });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+// Define the port to run the server
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
