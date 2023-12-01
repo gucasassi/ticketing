e5c739d59c671c849fd2707c6a2ac8e0cb4d0c58
Guillem Casas <gucasassi@gmail.com>
2023-12-01 23:48:48 +0100

    chore(back): configure colors on auth service

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -1,9 +1,13 @@
 // Imports
 import dotenv from 'dotenv';
+import colors from 'colors';
 import express from 'express';
 
 // Load env variables
 dotenv.config();
+
+// Enable use of colors on the CLI
+colors.enable();
 
 // Initialize expres app
 const app = express();
@@ -18,5 +22,5 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`.yellow);
 });
